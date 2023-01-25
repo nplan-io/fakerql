81cc141c23cd58b23784ae4e9f3a11627ba8bd42
Lawrence-nplan <lawrence@nplan.io>
2023-01-25 09:34:43 +0000

    Removing unused stuff

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
 import { createSchema, createYoga } from 'graphql-yoga'
 import * as fs from 'fs'
 import * as path from 'path'
-import { formatError } from 'apollo-errors'
-import * as jwt from 'express-jwt'
 import * as faker from 'faker/locale/en'
-import * as compression from 'compression'
 
 import resolvers from './resolvers'
-import defaultPlaygroundQuery from './initQuery'
 import { createServer } from 'http'
 
 const { JWT_SECRET } = process.env
@@ -33,27 +29,3 @@ const server = createServer(yoga)
 server.listen(4000, () => {
   console.info('Server is running on http://localhost:4000/graphql')
 })
-
-// server.express.disable('x-powered-by')
-
-// server.express.use(
-//   '/graphql',
-//   jwt({
-//     secret: JWT_SECRET,
-//     credentialsRequired: false,
-//   }),
-// )
-
-// server.express.use(compression())
-
-// const options = {
-//   formatError,
-//   endpoint: '/graphql',
-//   subscriptions: '/subscriptions',
-//   playground: '/',
-//   defaultPlaygroundQuery,
-// }
-
-// server.start(options, ({ port }) =>
-//   console.log(`Server is running on PORT: ${port}`),
-// )
